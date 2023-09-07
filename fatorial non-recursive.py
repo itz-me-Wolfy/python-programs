1ad6e01b937ba468b6c73ac9726d8040c775857c
Wolfy <105211191+itz-me-Wolfy@users.noreply.github.com>
2023-09-07 22:34:54 +0530

    Add files via upload

---
+++ fatorial non-recursive.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+num = int(input("Enter a positive integer: "))
+print("Factorial of", num, "is", factorial(num))
